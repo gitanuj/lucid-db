95290f2dd379eb606371a24d0799335dab42ea17
Aviral Takkar <aviraltakkar@gmail.com>
2016-02-27 17:36:22 -0800

    add exception

---
+++ src/com/lucid/spanner/NoCoordinatorException.java
@@ -0,0 +1,10 @@
+package com.lucid.spanner;
+
+/**
+ * Created by aviral on 2/27/16.
+ */
+public class NoCoordinatorException extends Exception {
+    public NoCoordinatorException(){
+        super();
+    }
+}
