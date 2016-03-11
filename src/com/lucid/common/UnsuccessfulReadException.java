aeb61776b95a123168d34d7d61fe4a7cf0548f03
Aviral Takkar <aviraltakkar@gmail.com>
2016-03-11 00:42:00 -0800

    add initial implementation for executeQuery. (untested)

---
+++ src/com/lucid/common/UnsuccessfulReadException.java
@@ -0,0 +1,7 @@
+package com.lucid.common;
+
+public class UnsuccessfulReadException extends Exception{
+    public UnsuccessfulReadException(String message){
+        super(message);
+    }
+}
--- src/com/lucid/rc/RCClient.java
+++ src/com/lucid/rc/RCClient.java
@@ -7,39 +7,57 @@ import io.atomix.copycat.Query;
 
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
 
 public class RCClient implements YCSBClient {
 
     private static final String LOG_TAG = "RC_CLIENT";
+    private ReadMajoritySelector readMajoritySelector;
+    private Object readsWaitOnMe;
+    private int readThreadsCounter;
+    private int readsSuccessfullyReturned;
 
     @Override
     public String executeQuery(Query query) throws Exception {
 
         List<AddressConfig> queryShards = Utils.getReplicaClusterIPs(((ReadQuery)query).key());
         Thread[] clientRequests = new Thread[queryShards.size()];
-        Object waitOnMe = new Object();
-        int counter = 0;
+        readsWaitOnMe = new Object();
+        readThreadsCounter = 0;
+        readMajoritySelector = new ReadMajoritySelector();
+        readsSuccessfullyReturned = 0;
 
         // Execute query on all servers, and wait for a response from majority.
         for(AddressConfig shard : queryShards){
             try{
-                clientRequests[counter] = new Thread(new QueryCluster(shard, (ReadQuery)query));
-                clientRequests[counter].start();
-                counter++;
+                clientRequests[readThreadsCounter] = new Thread(new QueryCluster(shard, (ReadQuery)query));
+                clientRequests[readThreadsCounter].start();
+                readThreadsCounter++;
             }
             catch(Exception e){
                 LogUtils.debug(LOG_TAG, "Transaction ID: " + ((ReadQuery)query).key() + ". Something went wrong while" +
-                        " starting thread number " + (counter - 1),e);
+                        " starting thread number " + (readThreadsCounter - 1),e);
             }
         }
 
-        LogUtils.debug(LOG_TAG, "Number of requests made: " + counter);
+        LogUtils.debug(LOG_TAG, "Number of requests made: " + readThreadsCounter);
 
-        // Sleep till notified when a majority of requests have come back.
-        waitOnMe.wait();
+        // Sleep till notified when a majority of requests have come back, or more than 10 seconds have elapsed.
+        readsWaitOnMe.wait(10 * 1000);
 
+        // Interrupt all running read threads.
+        for(int i = 0; i < readThreadsCounter; i++)
+            if(clientRequests[i].isAlive())
+                clientRequests[i].interrupt();
 
+        if(readsSuccessfullyReturned < readThreadsCounter / 2) // Read unsuccessful because majority of read threads
+            // did not return.
+            throw new UnsuccessfulReadException("Read unsuccessful because majority of threads did not return.");
+
+        return latestVersionRead();
     }
 
     @Override
@@ -47,11 +65,25 @@ public class RCClient implements YCSBClient {
         return false;
     }
 
+    private String latestVersionRead(){
+        long highestVersion = -1;
+        String answer = null;
+        for(Map.Entry<String, Long> map : readMajoritySelector.getMap().entrySet()){
+            if(map.getValue() > highestVersion) {
+                answer = map.getKey();
+                highestVersion = map.getValue();
+            }
+        }
+        return answer;
+    }
+
     private class QueryCluster implements Runnable {
 
         AddressConfig server;
         ReadQuery query;
         ObjectOutputStream writer;
+        Scanner reader;
+        String[] result;
 
         public QueryCluster(AddressConfig server, ReadQuery query) {
             this.server = server;
@@ -64,6 +96,13 @@ public class RCClient implements YCSBClient {
                 Socket socket = new Socket(server.host(), server.getClientPort());
                 writer = new ObjectOutputStream(socket.getOutputStream());
                 writer.writeObject(new TransportObject(Config.TXN_ID_NOT_APPLICABLE, query.key()));
+
+                // Wait for response.
+                reader = new Scanner(socket.getInputStream());
+                result = reader.next().split(Config.DELIMITER);
+
+                // Report to ReadMajoritySelector object.
+                readMajoritySelector.threadReturned(result[0], Long.parseLong(result[1]));
             }
             catch(Exception e){
                 LogUtils.debug(LOG_TAG, "Error in talking to server.", e);
@@ -71,10 +110,25 @@ public class RCClient implements YCSBClient {
         }
     }
 
+    private class ReadMajoritySelector {
 
-//    class CentralPool{
-//        table[value][version]
-//
-//        add enrty to tabel
-//    }
+        private HashMap<String, Long> map;
+
+        ReadMajoritySelector(){
+            map = new HashMap<>();
+        }
+
+        public HashMap<String, Long> getMap() {
+            return map;
+        }
+
+        public synchronized void threadReturned(String value, long version){
+            map.put(value, version);
+            readsSuccessfullyReturned++;
+
+            // If majority threads have returned, notify readsWaitOnMe object.
+            if(readsSuccessfullyReturned > readThreadsCounter / 2)
+                readsWaitOnMe.notify();
+        }
+    }
 }
