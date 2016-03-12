d648c1a395ad58e0503e7ae2976fb19d7d7eedb3
Tanuj Mittal <tanuj.183@gmail.com>
2016-03-12 15:15:54 -0800

    Fix Batcher

--- src/com/lucid/ycsb/Batcher.java
+++ src/com/lucid/ycsb/Batcher.java
@@ -19,7 +19,7 @@ public class Batcher {
 
     private static final int MAX_BATCH_SIZE = 5;
 
-    private static final long BATCH_EXPIRY = 100; //ms
+    private static final long BATCH_EXPIRY = 50; //ms
 
     private static final long REQUEST_TIMEOUT = 15 * 1000; //ms
 
@@ -38,10 +38,8 @@ public class Batcher {
 
     private final List<WriteObject> batch = new ArrayList<>();
 
-    // Txn ID vs List of monitor objects storing thread ID
-    private final Map<Long, List<Monitor>> waitingList = new HashMap<>();
+    private final Map<Long, Semaphore> waitingLocks = new HashMap<>();
 
-    // Thread ID vs Result map
     private final Map<Long, Boolean> resultMap = new HashMap<>();
 
     private YCSBClient ycsbClient;
@@ -62,108 +60,80 @@ public class Batcher {
     }
 
     public boolean write(WriteObject writeObject) {
-        long txnId = addToBatch(writeObject);
         try {
+            long txnId = addToBatch(writeObject);
             waitToComplete(txnId);
+            return checkResult(txnId);
         } catch (Exception e) {
-            LogUtils.error(LOG_TAG, "Something went wrong while waiting", e);
+            LogUtils.error(LOG_TAG, "Something went wrong while batching write", e);
             return false;
         }
-        return checkResult();
     }
 
     private long addToBatch(WriteObject writeObject) {
         synchronized (batch) {
-            processBatchIfRequired();
-            batch.add(writeObject);
-            return txnIdSequence;
-        }
-    }
+            Runnable task = () -> {
+                synchronized (batch) {
+                    BatchProcessor batchProcessor = new BatchProcessor(txnIdSequence, new ArrayList<>(batch));
+                    batch.clear();
+                    txnIdSequence++;
 
-    private void processBatchIfRequired() {
-        Runnable task = () -> {
-            synchronized (batch) {
-                BatchProcessor batchProcessor = new BatchProcessor(txnIdSequence, new ArrayList<>(batch));
-                batch.clear();
-                txnIdSequence++;
+                    final Future handler = executorService.submit(batchProcessor);
+                    timeoutExecutor.schedule((Runnable) () -> {
+                        try {
+                            handler.cancel(true);
+                        } catch (Exception e) {
+                            LogUtils.error(LOG_TAG, "Failed to cancel BatchProcessor on timeout", e);
+                        }
+                    }, REQUEST_TIMEOUT, TimeUnit.MILLISECONDS);
+                }
+            };
 
-                final Future handler = executorService.submit(batchProcessor);
-                timeoutExecutor.schedule((Runnable) () -> {
-                    try {
-                        handler.cancel(true);
-                    } catch (Exception e) {
-                        LogUtils.error(LOG_TAG, "Failed to cancel BatchProcessor on timeout", e);
-                    }
-                }, REQUEST_TIMEOUT, TimeUnit.MILLISECONDS);
+            // Remove any expiry tasks
+            if (lastBatchExpiryTask != null) {
+                lastBatchExpiryTask.cancel(true);
+                lastBatchExpiryTask = null;
             }
-        };
 
-        // Remove any expiry tasks
-        if (lastBatchExpiryTask != null) {
-            lastBatchExpiryTask.cancel(true);
-            lastBatchExpiryTask = null;
-        }
-
-        synchronized (batch) {
             if (batch.size() < MAX_BATCH_SIZE) {
                 lastBatchExpiryTask = batchExpiryExecutor.schedule(task, BATCH_EXPIRY, TimeUnit.MILLISECONDS);
             } else {
                 task.run();
             }
+
+            // Create semaphore
+            if (waitingLocks.get(txnIdSequence) == null) {
+                waitingLocks.put(txnIdSequence, new Semaphore(0));
+            }
+
+            batch.add(writeObject);
+            return txnIdSequence;
         }
     }
 
     private void waitToComplete(long txnId) throws InterruptedException {
-        final Monitor monitor = new Monitor(Thread.currentThread().getId());
-        synchronized (waitingList) {
-            List<Monitor> monitorList = waitingList.get(txnId);
-            if (monitorList == null) {
-                monitorList = new ArrayList<>();
-                waitingList.put(txnId, monitorList);
-            }
-            monitorList.add(monitor);
-        }
+        waitingLocks.get(txnId).acquire();
+    }
 
-        synchronized (monitor) {
-            monitor.wait();
+    private void notifyCompletion(long txnId, boolean result, int numWaitingThreads) {
+        synchronized (resultMap) {
+            resultMap.put(txnId, result);
+            waitingLocks.remove(txnId).release(numWaitingThreads);
         }
     }
 
-    private void notifyCompletion(long txnId, boolean result) {
-        List<Monitor> monitorList;
-        synchronized (waitingList) {
-            monitorList = waitingList.remove(txnId);
-        }
-
-        if (monitorList == null) {
-            return;
-        }
-
+    private boolean checkResult(long txnId) {
         synchronized (resultMap) {
-            for (Monitor monitor : monitorList) {
-                resultMap.put(monitor.getThreadId(), result);
-                synchronized (monitor) {
-                    monitor.notify();
-                }
-            }
-        }
-    }
-
-    private boolean checkResult() {
-        synchronized (resultMap) {
-            Boolean result = resultMap.remove(Thread.currentThread().getId());
-            if (result == null) {
-                return false;
-            }
-            return result;
+            // TODO When to remove result from map?
+            return resultMap.get(txnId);
         }
     }
 
     private class BatchProcessor implements Runnable {
 
-        private long txnId;
+        private final long txnId;
 
-        private List<WriteObject> batch;
+        private final List<WriteObject> batch;
 
         public BatchProcessor(long txnId, List<WriteObject> writeObjects) {
             this.txnId = txnId;
@@ -172,6 +142,8 @@ public class Batcher {
 
         @Override
         public void run() {
+            LogUtils.debug(LOG_TAG, "Starting request for txnId: " + txnId);
+
             boolean result = false;
             try {
                 WriteCommand writeCommand = new WriteCommand(txnId, YCSBUtils.toCommandsMap(batch));
@@ -181,7 +153,7 @@ public class Batcher {
             }
 
             try {
-                notifyCompletion(txnId, result);
+                notifyCompletion(txnId, result, batch.size());
             } catch (Exception e) {
                 LogUtils.error(LOG_TAG, "Failed to notify completion", e);
             }
