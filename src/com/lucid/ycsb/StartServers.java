8c035e91427a26b19cf71b5c060a49c2728fd249
Aviral Takkar <aviraltakkar@gmail.com>
2016-03-16 19:32:48 -0700

    rebuild jar

--- src/com/lucid/ycsb/StartServers.java
+++ src/com/lucid/ycsb/StartServers.java
@@ -4,6 +4,7 @@ import com.lucid.common.AddressConfig;
 import com.lucid.common.Config;
 import com.lucid.common.Utils;
 import com.lucid.rc.RCServer;
+import com.lucid.spanner.SpannerServer;
 
 public class StartServers {
 
@@ -11,11 +12,17 @@ public class StartServers {
 
         for (int i = 0; i < Config.SERVER_IPS.size(); i++) {
             final int index = i;
+            if((Integer.parseInt(args[0])) != Config.SPANNER)
+                Utils.startThreadWithName(() -> {
+                    AddressConfig config = Config.SERVER_IPS.get(index);
+                    new RCServer(config, index);
+                }, "server-" + i);
+            else
+                Utils.startThreadWithName(() -> {
+                    AddressConfig config = Config.SERVER_IPS.get(index);
+                    new SpannerServer(config, index);
+                }, "server-" + i);
 
-            Utils.startThreadWithName(() -> {
-                AddressConfig config = Config.SERVER_IPS.get(index);
-                new RCServer(config, index);
-            }, "server-" + i);
         }
     }
 }
